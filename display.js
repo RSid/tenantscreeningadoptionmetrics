8c3f7f2f9ac6bcf69134c60bdf60257411939b24
AllaHoffman <sm1th303@gmail.com>
2014-10-17 17:38:22 -0400

    Create display.js

---
+++ display.js
@@ -0,0 +1,14 @@
+screeningData = [{date:"10/2014", applicants: 51630, moveins: 96253, screenings: 11141}];
+
+//alert("hi");
+var datatable = function() {
+
+  for (i = 0; i < screeningData.length; i++) {
+      $("#data").append("<tr><td>"+ screeningData[i]['date'] +
+                    "</td><td>"+ screeningData[i]['applicants'] +
+                    "</td><td>"+ screeningData[i]['moveins'] +
+                    "</td><td>"+ screeningData[i]['screenings'] +"</td></tr>");
+              }
+};
+
+$(document).ready(datatable);
